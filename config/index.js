648ad8f4575f8f889f7d013b2b8e4d409d768d6f
Yorkie Liu <yorkiefixer@gmail.com>
2018-04-10 19:32:09 +0800

    disable hot reload

--- config/index.js
+++ config/index.js
@@ -19,7 +19,7 @@ module.exports = {
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
-    
+
     /**
      * Source Maps
      */
